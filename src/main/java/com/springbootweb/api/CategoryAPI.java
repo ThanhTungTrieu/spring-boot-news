b51a6d8b788188b364afc27657c8d5b005c802de
ThanhTungTrieu <thanhtungcb.2001@gmail.com>
2021-06-07 14:26:05 +0700

    @Transactional added

--- src/main/java/com/springbootweb/api/CategoryAPI.java
+++ src/main/java/com/springbootweb/api/CategoryAPI.java
@@ -3,6 +3,7 @@ package com.springbootweb.api;
 import com.springbootweb.dto.CategoryDTO;
 import com.springbootweb.service.ICategoryService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -12,16 +13,19 @@ public class CategoryAPI {
     private ICategoryService categoryService;
 
     @PostMapping(value = "/category")
+    @Transactional
     public CategoryDTO createCategory(@RequestBody CategoryDTO categoryDTO) {
         return categoryService.save(categoryDTO);
     }
 
     @PutMapping(value = "/category")
+    @Transactional
     public CategoryDTO updateCategory(@RequestBody CategoryDTO updatedCategoryDTO) {
         return categoryService.save(updatedCategoryDTO);
     }
 
     @DeleteMapping(value = "/category")
+    @Transactional
     public void deleteCategory(@RequestBody long[] ids) {
         categoryService.delete(ids);
     }
