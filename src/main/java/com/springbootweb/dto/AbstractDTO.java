eeab5fefe8a7d6bc0fac036dd1ca336fe35c4ea7
ThanhTungTrieu <thanhtungcb.2001@gmail.com>
2021-07-05 11:37:37 +0700

    change to generic class and add results(List)

--- src/main/java/com/springbootweb/dto/AbstractDTO.java
+++ src/main/java/com/springbootweb/dto/AbstractDTO.java
@@ -3,17 +3,19 @@ package com.springbootweb.dto;
 import lombok.Getter;
 import lombok.Setter;
 
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 
 @Getter
 @Setter
-public class AbstractDTO {
+public class AbstractDTO<T> {
 
     private Long id;
     private String createdBy;
     private Date createdDate;
     private String modifiedBy;
     private Date modifiedDate;
-
+    private List<T> results = new ArrayList<>();
 
 }
